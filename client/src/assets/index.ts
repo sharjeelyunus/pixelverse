2305073aeb88fd01a17f4d6c1e7d3684425305c8
Sharjeel Yunus <sharjeel924@gmail.com>
2023-01-28 04:35:50 +0500

    first commit

---
+++ client/src/assets/index.ts
@@ -0,0 +1,5 @@
+import download from './download.png';
+import logo from './PixelVerse Logo.svg';
+import preview from './preview.png';
+
+export { download, logo, preview };
---
+++ client/src/pages/CreatePost.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const CreatePost = (props: Props) => {
+  return <div>CreatePost</div>;
+};
+
+export default CreatePost;
