c2074a7f0d8acbeab8bced56bb1d6a24bf2a22bc
Sharjeel Yunus <sharjeel924@gmail.com>
2023-01-29 02:07:49 +0500

    generate ai images via api

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -15,7 +15,28 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {};
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const respnse = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
+        const data = await respnse.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please enter a prompt');
+    }
+  };
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
