5e8ce9ffaacb3250b696cb9728feb9544a37d7fd
Sharjeel Yunus <sharjeel924@gmail.com>
2023-01-29 02:04:20 +0500

    fix backend {dalleRoutes}

--- server/src/routes/dalleRoutes.ts
+++ server/src/routes/dalleRoutes.ts
@@ -7,18 +7,19 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 router.route('/').get((req, res) => {
-  res.send('Dalle works!');
+  res.status(200).json({ message: 'Hello from DALL-E!' });
 });
 
-router.route('/generate').post(async (req, res) => {
+router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
+
     const aiResponse = await openai.createImage({
       prompt,
       n: 1,
@@ -29,10 +30,10 @@ router.route('/generate').post(async (req, res) => {
     const image = aiResponse.data.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res
       .status(500)
-      .send(error?.response?.data?.error?.message || 'Server error');
+      .send(error?.response.data.error.message || 'Something went wrong');
   }
 });
 
