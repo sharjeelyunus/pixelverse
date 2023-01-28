9712efbed02b60fa7b1c67fd21ad3ce8cf95cda4
Sharjeel Yunus <sharjeel924@gmail.com>
2023-01-29 03:55:51 +0500

    upload images to cloudinary & send data to mongodb

--- server/src/routes/dalleRoutes.ts
+++ server/src/routes/dalleRoutes.ts
@@ -23,7 +23,7 @@ router.route('/').post(async (req, res) => {
     const aiResponse = await openai.createImage({
       prompt,
       n: 1,
-      size: '1024x1024',
+      size: '512x512',
       response_format: 'b64_json',
     });
 
--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
 
@@ -7,5 +7,53 @@ import Post from '../mongodb/models/post';
 dotenv.config();
 
 const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// GET All Posts
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'Fetching posts failed, please try again',
+    });
+  }
+});
+
+// Create A Post
+router.route('/').post(async (req: Request, res: Response) => {
+  try {
+    const { name, prompt, photo } = req.body;
+
+    if (!name || !prompt || !photo) {
+      return res.status(400).json({
+        success: false,
+        message: 'Please provide a name, prompt, and photo',
+      });
+    }
+
+    const photoUrl = (await cloudinary.uploader.upload(photo)).url;
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'Unable to create a post, please try again',
+    });
+  }
+});
 
 export default router;
